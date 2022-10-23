99852e7544a6c23546bbf6fc6eaea7165d7ced0b
Vedant Koditkar <vedant.koditkar@outlook.com>
2022-10-24 00:50:08 +0530

    Add scripts to install software

---
+++ win-software/setup.ps1
@@ -0,0 +1,9 @@
+#!/bin/pwsh
+winget install Microsoft.PowerShell
+winget install Microsoft.PowerToys --source winget
+winget install JanDeDobbeleer.OhMyPosh
+winget install Mozilla.Firefox
+winget install Microsoft.VisualStudioCode
+winget install Git.Git
+winget install 7zip.7zip
+winget install Microsoft.DotNet.SDK.6
