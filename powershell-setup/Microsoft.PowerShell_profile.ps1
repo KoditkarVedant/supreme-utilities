1f4b781a280a16fc403bfd95c14176dde3a8daa7
Vedant Koditkar <18693839+KoditkarVedant@users.noreply.github.com>
2025-12-06 15:13:22 +0530

    Powershell: Add auto completion

--- powershell-setup/Microsoft.PowerShell_profile.ps1
+++ powershell-setup/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,5 @@
-# oh-my-posh --init --shell pwsh --config "~\AppData\Local\Programs\oh-my-posh\themes\rudolfs-light.omp.json" | Invoke-Expression
 oh-my-posh --init --shell pwsh --config "~\AppData\Local\Programs\oh-my-posh\themes\tonybaloney.omp.json" | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
-# oh-my-posh --init --shell pwsh --config "c:\github\prompt\ohmyposh.omp.json" | Invoke-Expression
-
-function MoveToAWS {
-    Set-Location "D:\Vedant\AWS"
-}
+Set-PSReadLineOption -PredictionSource History -EditMode Windows -PredictionViewStyle ListView
